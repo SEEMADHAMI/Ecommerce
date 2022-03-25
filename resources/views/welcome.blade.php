0b9bd9f66b655f982dd85f8750645f68005a3b7a
Seema Dhami <75735909+SEEMADHAMI@users.noreply.github.com>
2022-03-25 17:08:06 +0545

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,432 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">git
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-commerce</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Akaya+Telivigala&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+</head>
+
+<body>
+    <header>
+        <!-- nav1 start -->
+        <nav class="nav1">
+
+            <div class="left">
+                <span>Welcome to my E-shop</span>
+                <a href="/signin">SIGNUP</a> OR
+
+                <a href="/login">LOGIN</a>
+            </div>
+            <div class="right">
+                <i class="fa fa-facebook"></i>
+                <i class="fa fa-instagram"></i>
+                <i class="fa fa-whatsapp"></i>
+                <i class="fa fa-twitter"></i>
+                <i class="fa fa-pinterest"></i>
+            </div>
+        </nav>
+        <!-- nav1 end -->
+
+        <!-- nav2 start -->
+        <nav class="nav2">
+            <div class="nav2-left">
+                <i class="fa fa-phone"></i>9800003001
+                <i class="fa fa-envelope"></i> nd1234@gmail.com
+            </div>
+            <div class="nav2-center">
+                <h1>Let'<span>share</span></h1>
+                <p>BUY BORROW SELL</p>
+            </div>
+            <div class="nav2-right">
+                <i class="fa fa-map"> Contact Us</i>
+            </div>
+        </nav>
+        <!-- nav2 end -->
+
+        <!-- nav3 start -->
+        <nav class="nav3">
+            <ul>
+                <li><a href="/welcome"><i class="fa fa-home"></i>HOME</a></li>
+                <li><a href="#"><i class="fa fa-user"></i>RENT A DRESS</a></li>
+                <li><a href="#"><i class="fa fa-home"></i>FIND A DRESS</a></li>
+                <li><a href="#"><i class="fa fa-user"></i>OCCASIONS</a></li>
+                <li><a href="#"><i class="fa fa-calendar-check-o"></i>WHATS NEW</a></li>
+                <li><a href="#"><i class="fa fa-anchor"></i>FEATURES<i class="fa fa-caret-down"></i></a>
+                    <ul>
+                        <li>
+                            <a href="#"><i class="fa fa-clipboard">PAGES</i></a>
+                            <a href="#"><i class="fa fa-clipboard">TOOLS</i></a>
+                            <a href="#"><i class="fa fa-clipboard">SERVICE</i></a>
+                            <a href="#"><i class="fa fa-clipboard">PRIVACY POLICY</i></a>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="/Cart"><i class="fa fa-users"></i>Add to Cart</a></li>
+                <li><a href="#"><i class="fa fa-search"></i>Search</a></li>
+            </ul>
+        </nav>
+
+
+        <!-- nav3 end -->
+
+
+
+        <!-- feature start -->
+
+        <section class="featured">
+            <div class="featured-text">
+                <button>FOR ANY OCCASIONS</button>
+                <h2>BUR BORROW SELL</h2>
+
+            </div>
+        </section>
+    </header>
+    <!-- lateststart -->
+    <section class="latest">
+        <div class="product-intro">
+            <h1>New <span>Arrivals</span></h1>
+            <p>Lets'share fashion</p>
+        </div>
+        <div class="card-container">
+            <div class="card">
+                <div>
+                    <img src="img/first.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="img/first.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="img/first.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="img/first.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+
+            <div class="card">
+                <div>
+                    <img src="img/first.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+        </div>
+
+        <div class="card-container">
+            <div class="card">
+                <div>
+                    <img src="img/download1.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="img/download1.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="img/download1.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="img/download1.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+
+            <div class="card">
+                <div>
+                    <img src="img/download1.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+    </section>
+
+    <!-- Popular Products start -->
+    <section class="latest">
+        <div class="product-intro">
+            <h1>Popular <span>Products</span></h1>
+            <p>Lets'share fashion</p>
+        </div>
+        <div class="card-container">
+            <div class="card">
+                <div>
+                    <img src="img/download2.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="img/download2.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="img/download2.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+            <div class="card">
+                <div>
+                    <img src="img/download2.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+
+            <div class="card">
+                <div>
+                    <img src="img/download2.jpg" alt="">
+                </div>
+                <h1>Branded Suits</h1>
+                <span>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+            </span>
+                <p>$500.00</p>
+                <button>Add to Cart</button>
+            </div>
+
+    </section>
+    <!-- Popular Product end -->
+    <section class="trends">
+        <div class="product-intro">
+            <h1>Top<span>Trends</span></h1>
+            <p>Lets'share fashion</p>
+        </div>
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="img/crackjack1.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="img/crackjack1.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="img/crackjack1.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="img/crackjack1.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="img/crackjack1.jpg" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="img/crackjack1.jpg" />
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+    </section>
+
+    <footer>
+        <div>
+            <h1>Let'<span>Share</span></h1>
+            <p>BUY BORROW SELL</p>
+        </div>
+        <div>
+            <i class="fa fa-facebook"></i>
+            <i class="fa fa-instagram"></i>
+            <i class="fa fa-whatsapp"></i>
+            <i class="fa fa-twitter"></i>
+            <i class="fa fa-linkedin"></i>
+            <i class="fa fa-pinterest"></i>
+            <p>Design your own dress</p>
+        </div>
+        <div>
+            <p>@CopyRights Are All reserved</p>
+        </div>
+    </footer>
+
+
+
+
+
+
+
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js "></script>
+    <script>
+        var swiper = new Swiper(".mySwiper ", {
+            effect: "coverflow ",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto ",
+            coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 1200,
+                modifier: 1,
+                slideShadows: false,
+            },
+            pagination: {
+                el: ".swiper-pagination ",
+            },
+        });
+    </script>
+</body>
+
+</html>
